dc04dda4ab387921b780bb55d1361de06d0ebc22
Mbilse <mbilse001@outlook.com>
2025-01-25 16:48:58 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String []args) {
+        System.out.println("Windows正在启动…");
+        System.out.println("这andows也太假了吧！");
+        System.out.println("谁来帮我编一下手机XP？");
+        System.out.println("我先离开了。");/*运行结束*/
+    }
+}
